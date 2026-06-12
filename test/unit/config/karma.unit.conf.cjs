13469160afc43fcc3876e3c6c3bf397593d0cf32
Robert Bryer <robert.bryer@bbc.co.uk>
2026-06-12 12:21:03 +0100

    Remove firefox from unit tests for mysterious failures on ci

--- test/unit/config/karma.unit.conf.cjs
+++ test/unit/config/karma.unit.conf.cjs
@@ -118,7 +118,7 @@ module.exports = function (config) {
 
         // start these browsers
         // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-        browsers: ['Chrome_for_docker', 'FirefoxHeadless'],
+        browsers: ['Chrome_for_docker'],
 
         // Continuous Integration mode
         // if true, Karma captures browsers, runs the tests and exits
